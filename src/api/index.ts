3def6d69b62cfb657cbfdba9995da38d0d5dd746
Dmitry <dmitry2002savenkov@mail.ru>
2022-08-22 12:27:19 +0300

    coincap-[fe]-created request and simple layout

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,1 +1,13 @@
 export const API_URL = 'https://api.coincap.io/v2/assets';
+
+export const fetchAllCyrrencies = async ({
+  offset,
+  limit = 10,
+}: {
+  offset: number;
+  limit: number;
+}) => {
+  return fetch(`${API_URL}?offset=${offset}&limit=${limit}`).then((res) => {
+    return res.json();
+  });
+};
--- src/screens/Cryptocurrencies.tsx
+++ src/screens/Cryptocurrencies.tsx
@@ -1,6 +1,6 @@
 // Lib
 import React, { useContext } from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, FlatList } from 'react-native';
 
 import { AppContext } from '../store';
 
@@ -10,11 +10,27 @@ import Loader from '../components/Loader';
 const Cryptocurrencies = () => {
   const {
     state: {
-      cryptoCurrencies: { loading },
+      cryptoCurrencies: { loading, data },
     },
   } = useContext(AppContext);
 
-  return <SafeAreaView>{!loading ? <Loader /> : null}</SafeAreaView>;
+  return (
+    <SafeAreaView>
+      {loading ? (
+        <Loader />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={(item, index) => index}
+          renderItem={({ item, index }) => (
+            <View>
+              <Text>Currency</Text>
+            </View>
+          )}
+        />
+      )}
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({});
