93b8eda7c37bd7d343180bfdf4bec3d72db0e1ad
Dmitry <dmitry2002savenkov@mail.ru>
2022-08-22 12:40:38 +0300

    coincap-[fe]-updated flatlist and load data when scroll

--- src/screens/Cryptocurrencies.tsx
+++ src/screens/Cryptocurrencies.tsx
@@ -1,5 +1,5 @@
 // Lib
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect, useCallback } from 'react';
 import { StyleSheet, Text, View, SafeAreaView, FlatList } from 'react-native';
 
 import { AppContext } from '../store';
@@ -7,12 +7,21 @@ import { AppContext } from '../store';
 //Components
 import Loader from '../components/Loader';
 
+import { fetchAllCyrrencies } from '../api';
+
 const Cryptocurrencies = () => {
+  const [offset, setOffset] = useState(0);
+  const response = fetchAllCyrrencies(offset);
+
   const {
     state: {
       cryptoCurrencies: { loading, data },
     },
   } = useContext(AppContext);
+
+  const loadMore = useCallback(() => {
+    setOffset((offset) => offset + 10);
+  }, []);
 
   return (
     <SafeAreaView>
@@ -22,6 +31,8 @@ const Cryptocurrencies = () => {
         <FlatList
           data={data}
           keyExtractor={(item, index) => index}
+          onEndReached={loadMore}
+          onEndReachedThreshold={0.3}
           renderItem={({ item, index }) => (
             <View>
               <Text>Currency</Text>
