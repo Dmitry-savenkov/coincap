92637bb69287336bd7893abd01b6e25b1f8b7c4f
Dmitry <dmitry2002savenkov@mail.ru>
2022-08-22 12:07:31 +0300

    coincap-[fe]-added styles for loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,7 +1,17 @@
 // Lib
 import React from 'react';
-import { ActivityIndicator } from 'react-native';
+import { StyleSheet, ActivityIndicator } from 'react-native';
 
-const Loader = () => <ActivityIndicator size="large" />;
+const Loader = () => <ActivityIndicator size="large" style={[styles.loader]} />;
+
+const styles = StyleSheet.create({
+  loader: {
+    flex: 1,
+    justifyContent: 'center',
+    alignContent: 'center',
+    width: '100%',
+    height: '100%',
+  },
+});
 
 export default Loader;
